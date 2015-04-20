9dc9b457dc4b7e8043734b09fa5ca17aed3a373d
AlexPicaro <alexandre.drouin@outlook.com>
2015-04-20 15:05:49 -0400

    Added the latest model (Models/ConvNet8.py), a 2 layer convnet with dropout. Added all trained model pickel files in the Results directory, total training and best result for each model. Add the best model obtained (Best_model), which corresponds to Results/ConvNet1_best.pkl.

---
+++ Models/ConvNet8.py
@@ -0,0 +1,88 @@
+from ift6266h15.code.pylearn2.datasets.variable_image_dataset import DogsVsCats, RandomCrop
+from pylearn2.models.mlp import ConvRectifiedLinear, Softmax, MLP
+from pylearn2.space import Conv2DSpace
+from pylearn2.training_algorithms.sgd import SGD
+from pylearn2.costs.cost import SumOfCosts, MethodCost
+from pylearn2.costs.mlp.dropout	import Dropout
+from pylearn2.training_algorithms.learning_rule import Momentum, MomentumAdjustor
+from pylearn2.termination_criteria import EpochCounter, MonitorBased
+from pylearn2.train import Train
+from pylearn2.train_extensions.best_params import MonitorBasedSaveBest
+
+batchSize = 20
+scaledSize = 256
+cropSize = 200
+
+#Create datasets
+train = DogsVsCats(
+                     RandomCrop(scaledSize, cropSize),
+                     start = 0,
+                     stop = 19999
+                     )
+valid = DogsVsCats(
+                    RandomCrop(scaledSize, cropSize),
+                    start = 20000,
+                    stop = 22500
+                   )
+
+#Instantiate layers
+h0 = ConvRectifiedLinear(output_channels = 32, 
+                         kernel_shape = [5,5], 
+                         pool_shape = [2,2], 
+                         pool_stride = [2,2], 
+                         layer_name = "h0", 
+                         irange = 0.1, 
+                         border_mode = "full")
+ 
+h1 = ConvRectifiedLinear(output_channels = 32, 
+                         kernel_shape = [5,5], 
+                         pool_shape = [2,2], 
+                         pool_stride = [2,2], 
+                         layer_name = "h1", 
+                         irange = 0.1, 
+                         border_mode = "full")
+ 
+y = Softmax(n_classes = 2,
+            layer_name = "y",
+            irange = 0.1)
+
+inputSpace = Conv2DSpace(shape = [cropSize,cropSize],
+                         num_channels = 3)
+  
+model = MLP(layers = [h0, h1, y], 
+            batch_size = batchSize, 
+            input_space = inputSpace)
+ 
+algorithm = SGD(learning_rate = 1E-3, 
+                cost = SumOfCosts([
+                                   MethodCost("cost_from_X"),
+                                   Dropout(default_input_include_prob = 0.25,
+                                           default_input_scale = 1.3333)
+                                  ]), 
+                batch_size = batchSize, 
+                monitoring_batch_size = batchSize,
+                monitoring_dataset = {'train': train,
+                                      'valid':valid}, 
+                monitor_iteration_mode = "even_batchwise_shuffled_sequential", 
+                termination_criterion = EpochCounter(max_epochs = 200),
+                learning_rule = Momentum(init_momentum = 0.0),
+                train_iteration_mode = "even_batchwise_shuffled_sequential")
+     
+train = Train(dataset = train, 
+              model = model, 
+              algorithm = algorithm, 
+              save_path = "ConvNet8.pkl", 
+              save_freq = 1, 
+              extensions = [
+                            MonitorBasedSaveBest(channel_name = "valid_y_misclass",
+                                                 save_path = "ConvNet8_best.pkl"),
+			    MomentumAdjustor(final_momentum = 0.99,
+                                             start = 25,
+					     saturate = 125)
+                            ])
+     
+print("Starting training session")
+
+train.main_loop()
+
+print("Done!")
